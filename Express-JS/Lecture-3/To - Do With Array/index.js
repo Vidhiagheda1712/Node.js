ecc886e65ea7042d61811d331020c624fe71b36a
Vidhiagheda1712 <aghedavidhi1712@gmail.com>
2024-12-30 10:52:48 +0530

    30/12/2024

--- Express-JS/Lecture-3/To - Do With Array/index.js
+++ Express-JS/Lecture-3/To - Do With Array/index.js
@@ -37,6 +37,30 @@ app.get('/deleteuser', (req, res) => {
     record = deletedata;
     return res.redirect('/table');
 })
+
+app.get('/edituser', (req, res) => {
+    let id = req.query.id;
+    let single = record.find(val => val.id == id);
+    return res.render('edit', {
+        single
+    });
+})
+
+app.post('/updateuser', (req, res) => {
+    const { editid, username, useremail, userphone } = req.body;
+    let up = record.map((val) => {
+        if (val.id == editid) {
+            val.name = username
+            val.email = useremail
+            val.phone = userphone
+        }
+        return val;
+    })
+    record = up;
+    console.log(`Record Update Successfully....`);
+    return res.redirect('/table');
+})
+
 app.listen(port, (err) => {
     if (err) {
         console.log(err);
@@ -46,3 +70,5 @@ app.listen(port, (err) => {
     console.log(`Server is Start on Port :-http://localhost:${port}`);
 
 })
+
+
